5792244350f22fe1e53dd8dcdd1d274b9da9b2c7
Diyorbek7337 <Diyorbek8587@gmail.com>
2025-08-21 17:42:09 +0500

    Refactor Categories component to include styling and structured list of menu items

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 function Categories() {
   return (
-    <div>
+    <div className='categoriesBox'>
         <h2>Menu</h2>
+        <ul className='categoriesList'>
+            <li><a href="#burgers">Burger</a></li>
+            <li><a href="#lavash">Lavash</a></li>
+            <li><a href="#donar">Donar</a></li>
+            <li><a href="#hotdog">Hot-Dog</a></li>
+            <li><a href="#pitta">Pitta</a></li>
+            <li><a href="#kfc">KFC</a></li>
+            <li><a href="#chicken">Chicken</a></li>
+            <li><a href="#twister">Twister</a></li>
+            <li><a href="#haggi">Haggi</a></li>
+            <li><a href="#longer">Longer</a></li>
+            <li><a href="#barbekyu">Barbekyu</a></li>
+            <li><a href="#klab">Klab Sendvich</a></li>
+            <li><a href="#garnir">Garnir</a></li>
+            <li><a href="#pizza">Pizza</a></li>
+            <li><a href="#salat">Salat</a></li>
+            <li><a href="#drinks">Ichimliklar</a></li>
+            <li><a href="#icecream">Muzqaymoq</a></li>
+            <li><a href="#moxito">Moxito</a></li>
+            <li><a href="#vafli">Gongkong vafli</a></li>
+            <li><a href="#kokteyllar">Kokteyllar</a></li>
+        </ul>
+            
     </div>
   )
 }
